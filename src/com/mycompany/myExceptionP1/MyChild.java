01331262590594068fb6328faf9b1a40da5c4524
rja26 <rja26@njit.edu>
2017-01-03 23:47:35 -0500

    First

---
+++ src/com/mycompany/myExceptionP1/MyChild.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.mycompany.myExceptionP1;
+
+/**
+ * @author riddy
+ *
+ */
+public class MyChild extends MyParent {
+	@Override
+	public void method1(Integer _inp) throws MyException1Child {
+	}
+
+
+}
